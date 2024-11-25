20ae157bd47d69237d6d32bf96238fce2de182c6
Maksym Rudichenko <maksymr@lyft.com>
2024-11-25 21:03:45 +0200

    Add main runnable

---
+++ main.py
@@ -0,0 +1,56 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="File Compression System", add_help=False
+    )
+    subparsers = parser.add_subparsers(dest="command")
+
+    gui_parser = subparsers.add_parser("gui", help="Start GUI")
+
+    compress_parser = subparsers.add_parser("compress", help="Compress files")
+    compress_parser.add_argument(
+        "-a", "--algorithm", choices=["zip", "gzip", "lzma"], required=True
+    )
+    compress_parser.add_argument("-f", "--files", nargs="+", required=True)
+    compress_parser.add_argument("-o", "--output", required=True)
+    compress_parser.add_argument("-p", "--password")
+
+    decompress_parser = subparsers.add_parser("decompress", help="Decompress archive")
+    decompress_parser.add_argument("-f", "--file", required=True)
+    decompress_parser.add_argument("-o", "--output", required=True)
+    decompress_parser.add_argument("-p", "--password")
+
+    args = parser.parse_args()
+
+    if args.command == "gui":
+        from compressors.gui_compressor import GuiCompressor
+
+        app = GuiCompressor()
+        app.run()
+    elif args.command == "compress":
+        from compressors.cli_compressor import CliCompressor
+
+        app = CliCompressor(
+            files=args.files,
+            algorithm=args.algorithm,
+            output=args.output,
+            password=args.password,
+            direction="compress",
+        )
+        app.run()
+    elif args.command == "decompress":
+        from compressors.cli_compressor import CliCompressor
+
+        app = CliCompressor(
+            files=[args.file],
+            output=args.output,
+            password=args.password,
+            direction="decompress",
+        )
+        app.run()
+
+
+if __name__ == "__main__":
+    main()
