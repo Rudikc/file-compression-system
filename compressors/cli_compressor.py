89c377e63f55dfa8aa14ae09ab36f47f49a608d7
Maksym Rudichenko <maksymr@lyft.com>
2024-11-25 20:59:42 +0200

    Add implementation of cli compressor

---
+++ compressors/cli_compressor.py
@@ -0,0 +1,20 @@
+from compressors.compressor import Compressor
+
+
+class CliCompressor(Compressor):
+    def __init__(self, files, output, direction, password=None, algorithm=None):
+        super().__init__()
+        self.files = files
+        self.algorithm = algorithm
+        self.output = output
+        self.password = password
+        self.direction = direction
+
+    def run(self):
+        if self.direction == "compress":
+            self.compress(self.files, self.algorithm, self.output, self.password)
+        elif self.direction == "decompress":
+            self.decompress(self.files[0], self.output, self.password)
+        else:
+            print("Invalid direction. Please choose 'compress' or 'decompress'")
+            return
